9e932daa7c103741c7444083a22198031bf33ad2
drrajkumarmajoka-beep <drrajkumarmajoka@gmail.com>
2026-06-17 12:58:07 +0530

    Update login.js

---
+++ login.js
@@ -0,0 +1,34 @@
+import { auth } from "./firebase-config.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const form =
+  document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email =
+    document.getElementById("email").value;
+
+  const password =
+    document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    alert("Login Successful!");
+
+    window.location.href =
+      "index.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+});
